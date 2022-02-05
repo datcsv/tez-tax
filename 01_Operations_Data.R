877fdc5bb95b9f1dcf0039e422774e533a57c9ba
datcsv <datcsv@gmail.com>
2022-02-05 12:48:28 -0800

    House cleaning -
    -Variables now defined in configuration files
    -Moved functions to separate folder
    -Moved data to separate folder
    -Added date filters to the initial pulls

--- 01_Operations_Data.R
+++ 01_Operations_Data.R
@@ -1,47 +1,25 @@
 # Clear workspace
 rm(list=ls())
 
-# Import packages
-packages <- c("tidyverse", "jsonlite", "magrittr")
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-    install.packages(packages[!installed_packages])
-}
-invisible(lapply(packages, library, character.only=TRUE))
+# Load function
+source("functions/load_packages.R")
+source("functions/tzkt_operations.R")
+source("functions/tzkt_operations_hash.R")
 
-# User settings
-addresses <- c(
-  "tz1a2ZeWmyNQ8BiuFNTE4vmFEP9MBaP76QPX", # datcsv
-  "tz1L5vaycmTzEDekjDJSFZJ1V8FPwAUCVSDM", # datcsv1
-  "tz1Sbt4C1MXm1AWPK6qdgfDzciJCCUVADmUt", # datcsv2
-  "tz2TXkQS6kPPXbuVMsNBcdtpjWf9DPYWHfqV"  # gmail
-)
+# Load packages
+load_packages(c("tidyverse", "jsonlite", "magrittr"))
 
-# Define tzkt API functions
-tzkt_operations <- function(
-  address, level=NA, limit=NA, quote="usd", base="https://api.tzkt.io/"
-) {
-  sfx <- paste0("v1/accounts/", address, "/operations?quote=", quote)
-  if (!is.na(level)) sfx <- paste0(sfx, "&level.lt=", level)
-  if (!is.na(limit)) sfx <- paste0(sfx, "&limit=", limit)
-  url <- paste0(base, sfx)
-  x <- fromJSON(url)
-  return(x)
-}
-
-tzkt_operations_hash <- function(
-  hash, quote="usd", base="https://api.tzkt.io/"
-) {
-  sfx <- paste0("v1/operations/", hash, "?quote=", quote)
-  url <- paste0(base, sfx)
-  x <- fromJSON(url)
-  return(x)
-}
+# Load data
+load(file="data/addresses.RData")
+load(file="data/currency.RData")
+load(file="data/date_span.RData")
 
 # Download operations - first pass (wallet search)
 limit_ops <- 1000
 for (i in 1:length(addresses)) {
-  operations_i <- tzkt_operations(address=addresses[i], limit=limit_ops)
+  operations_i <- tzkt_operations(
+    address=addresses[i], limit=limit_ops, span=date_span, quote=currency
+  )
   while ((nrow(operations_i) %% limit_ops) == 0) {
     level <- min(operations_i$level + 1)
     operations_i <- bind_rows(
@@ -60,10 +38,7 @@ operations %<>%
 
 # Download operations - second pass (hash search)
 operations_hash <- operations %>% 
-  filter(., 
-    (target[[2]] %in% addresses) |
-    (parameter[[1]] == "conclude_auction")
-  ) %>%
+  filter(., target[[2]] %in% addresses) %>%
   distinct(., hash)
 
 for (i in 1:nrow(operations_hash)) {
@@ -88,5 +63,4 @@ operations %<>%
   distinct(.)
 
 # Export data
-save(addresses, file="addresses.RData")
-save(operations, file="operations.RData")
+save(operations, file="data/operations.RData")
