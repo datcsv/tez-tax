8d8e8a609c51c57e1d5eb3543308be8390de03ba
datcsv <datcsv@gmail.com>
2022-02-07 17:49:23 -0800

    Added OBJKT auction data for early contract

--- 01_Operations_Data.R
+++ 01_Operations_Data.R
@@ -57,9 +57,40 @@ for (i in 1:nrow(operations_hash)) {
   else operations2 %<>% bind_rows(., operations_i)
 }
 
+# Download contract data
+contracts <- c("KT1Dno3sQZwR5wUCWxzaohwuJwG3gX1VWj1Z")
+for (i in 1:length(contracts)) {
+  operations_i <- tzkt_operations(
+    address=contracts[i], limit=limit_ops, span=date_span, quote=currency
+  )
+  while ((nrow(operations_i) %% limit_ops) == 0) {
+    level <- min(operations_i$level + 1)
+    operations_i <- bind_rows(
+      operations_i,
+      tzkt_operations(address=contracts[i], level=level, limit=limit_ops)
+    )
+  }
+  if (i == 1) operations3 <- operations_i
+  else operations3 %<>% bind_rows(., operations_i)
+}
+
+# Clean contract data
+operations_hash <- operations3 %>% distinct(., hash)
+operations_temp <- operations3[0, ]
+for (i in 1:nrow(operations_hash)) {
+  operations_i <- filter(operations3, hash == operations_hash[i, ])
+  parameter_entry <- operations_i$parameter$entrypoint
+  parameter_value <- operations_i$parameter$value
+  token_receiver <- list_check(operations_i$parameter$value, "to_")
+  if (("swap" %in% parameter_entry > 0) & (token_receiver %in% addresses)) {
+    operations_temp %<>% bind_rows(., operations_i)
+  }
+}
+operations3 <- operations_temp
+
 # Combine and clean operations data
 operations %<>% 
-  bind_rows(., operations2) %>%
+  bind_rows(., operations2, operations3) %>%
   arrange(., id, hash) %>%
   distinct(.)
 
