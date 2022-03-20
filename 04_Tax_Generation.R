30dbc8c496afa66a1e4dffbb784b39c6f55b6168
datcsv <datcsv@gmail.com>
2022-03-20 16:59:15 -0700

    Working through tax form 8949

--- 04_Tax_Generation.R
+++ 04_Tax_Generation.R
@@ -2,14 +2,32 @@
 load(file="data/is_updated.RData")
 load(file="data/bs.RData")
 load(file="data/tax_8949.RData")
-load(file="data/xtzIncome_data")
+load(file="data/xtzIncome_data.RData")
 
 # Define form filepaths
 f8949 <- "forms/f8949.pdf"
 
+# Update form identification fields
 f8949_fields <- get_fields(input_filepath=f8949)
-f8949_fields$`topmostSubform[0].Page1[0].f1_1[0]`$value <- "Test"
+f8949_fields[[1]][[3]] <- legal_name
+f8949_fields[[2]][[3]] <- ssn
+f8949_fields[[5]][[3]] <- "3"
 
+# Update form tax form rows
+for (i in 1:14) {
+  
+  f8949_fields[[6 + (i - 1) * 8]][[3]]  <- "TEST"
+  f8949_fields[[7 + (i - 1) * 8]][[3]]  <- "TEST"
+  f8949_fields[[8 + (i - 1) * 8]][[3]]  <- "TEST"
+  f8949_fields[[9 + (i - 1) * 8]][[3]]  <- "TEST"
+  f8949_fields[[10 + (i - 1) * 8]][[3]] <- "TEST"
+  f8949_fields[[11 + (i - 1) * 8]][[3]] <- "TEST"
+  f8949_fields[[12 + (i - 1) * 8]][[3]] <- "TEST"
+  f8949_fields[[13 + (i - 1) * 8]][[3]] <- "TEST"
+  
+}
+
+# Generate PDF file
 set_fields(
   input_filepath=f8949,
   output_filepath=paste0("data/f8949_", ssn, ".pdf"),
