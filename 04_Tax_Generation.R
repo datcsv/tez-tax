a9991cb55a5b48d780c29ceee03bcddee80a9b4b
datcsv <datcsv@gmail.com>
2022-03-20 18:02:56 -0700

    ITERATE IT

--- 04_Tax_Generation.R
+++ 04_Tax_Generation.R
@@ -5,13 +5,19 @@ load(file="data/tax_8949.RData")
 load(file="data/xtzIncome_data.RData")
 
 # Testing data
-tax_8949 <- tax_8949[1:14, ]
+tax_8949 <- tax_8949[1:59, ]
 
-# Define form filepaths
+# Define template filepaths
 f8949 <- "forms/f8949.pdf"
+
+# Create pdf directories
+dir.create("data/pdf", showWarnings=FALSE)
 
 # Generate tax form 8949
 for (i in seq(1, nrow(tax_8949), by=14)) {
+  
+  if (i == 1) k <- 1
+  else k <- k + 1
 
   # Update form identification fields
   f8949_fields <- get_fields(input_filepath=f8949)
@@ -27,22 +33,23 @@ for (i in seq(1, nrow(tax_8949), by=14)) {
     f8949_fields[[9  + (j - 1) * 8]][[3]] <- sprintf("%.2f", tax_8949[[i + j - 1, 4]])
     f8949_fields[[10 + (j - 1) * 8]][[3]] <- sprintf("%.2f", tax_8949[[i + j - 1, 5]])
     f8949_fields[[11 + (j - 1) * 8]][[3]] <- tax_8949[[i + j - 1, 6]]
-    f8949_fields[[12 + (j - 1) * 8]][[3]] <- sprintf("%.2f", tax_8949[[i + j - 1, 7]])
+    if (!is.na(tax_8949[[i + j - 1, 7]])) {
+      f8949_fields[[12 + (j - 1) * 8]][[3]] <- sprintf("%.2f", tax_8949[[i + j - 1, 7]])
+    }
     f8949_fields[[13 + (j - 1) * 8]][[3]] <- sprintf("%.2f", tax_8949[[i + j - 1, 8]])
   }
   
   # Update capital gain/loss total fields
-  f8949_fields[[118]][[3]] <- sum(tax_8949[i:(i + 13), 4], na.rm=TRUE)
-  f8949_fields[[119]][[3]] <- sum(tax_8949[i:(i + 13), 5], na.rm=TRUE)
-  f8949_fields[[121]][[3]] <- sum(tax_8949[i:(i + 13), 7], na.rm=TRUE)
-  f8949_fields[[122]][[3]] <- sum(tax_8949[i:(i + 13), 8], na.rm=TRUE)
+  f8949_fields[[118]][[3]] <- sprintf("%.2f", sum(tax_8949[i:(i + 13), 4], na.rm=TRUE))
+  f8949_fields[[119]][[3]] <- sprintf("%.2f", sum(tax_8949[i:(i + 13), 5], na.rm=TRUE))
+  f8949_fields[[121]][[3]] <- sprintf("%.2f", sum(tax_8949[i:(i + 13), 7], na.rm=TRUE))
+  f8949_fields[[122]][[3]] <- sprintf("%.2f", sum(tax_8949[i:(i + 13), 8], na.rm=TRUE))
   
+  # Generate PDF file
+  set_fields(
+    input_filepath=f8949,
+    output_filepath=paste0("data/pdf/f8949_", ssn, "_", str_pad(k, 4, pad="0"), ".pdf"),
+    fields=f8949_fields,
+    overwrite=TRUE,
+  )
 }
-
-# Generate PDF file
-set_fields(
-  input_filepath=f8949,
-  output_filepath=paste0("data/f8949_", ssn, ".pdf"),
-  fields=f8949_fields,
-  overwrite=TRUE,
-)
