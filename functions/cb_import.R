bce7f3f425683171f4c1121fb3be9618488abc96
datcsv <98504102+datcsv@users.noreply.github.com>
2022-03-23 08:18:42 -0700

    Update cb_import.R

--- functions/cb_import.R
+++ functions/cb_import.R
@@ -66,6 +66,7 @@ for (i in 1:nrow(cb)) {
     x$xtzReceived   <- 0
     x$xtzProceeds   <- cb_i$`Total (inclusive of fees)`
     x$case          <- "Coinbase sell"
+    x$quote         <- cb_i$`Total (inclusive of fees)` / cb_i$`Quantity Transacted`
   }
   
   # Coinbase send
