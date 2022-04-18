d0539dccf9a2842e4f6f3fc3be7bfa1cf24fdbd0
datcsv <datcsv@gmail.com>
2022-04-18 13:15:20 -0700

    Added batch collect support

--- functions/classify_tx.R
+++ functions/classify_tx.R
@@ -109,24 +109,29 @@ rari_contracts <- c(
 )
 
 # Adjust batch transactions
-# op_hash <- "0"
-# j       <- 0
-# for (i in 1:nrow(operations)) {
-#   if (
-#     (operations$targetAddress[i] %in% hen_contracts) | 
-#     (operations$hash[i] == op_hash)
-#   ) {
-#     if (operations$hash[i] == op_hash) {
-#       if (is.na(operations$initiatorAddress[i])) {
-#          j <- j + 1
-#       }
-#     } else {
-#       op_hash <- operations$hash[i]
-#       j <- 0
-#     }
-#     operations$hash[i] <- paste0(op_hash, "_", j)
-#   }
-# }
+op_hash <- "0"
+j <- 0
+for (i in 1:nrow(operations)) {
+  if (
+    (operations$targetAddress[i] %in% hen_contracts) |
+    (operations$hash[i] == op_hash)
+  ) {
+    if (
+      (operations$hash[i] == op_hash)
+    ) {
+      if (
+        (is.na(operations$initiatorAddress[i])) & 
+        (sum(operations$parameterEntry[i] %in% "collect") > 0)
+      ) {
+         j <- j + 1
+      }
+    } else {
+      op_hash <- operations$hash[i]
+      j <- 0
+    }
+    operations$hash[i] <- paste0(op_hash, "_", j)
+  }
+}
 
 # Create null income statement
 is <- operations[0, ]
