c96f3d6a8d509924748a08815bccb8def87cb222
datcsv <datcsv@gmail.com>
2022-04-27 16:38:07 -0700

    Adjust balance sheet code for custom basis/proceeds

--- 03_BS_Generation.R
+++ 03_BS_Generation.R
@@ -135,7 +135,8 @@ for (i in 1:nrow(is)) {
     # Initialize variables
     xtzBalance  <- is_i$xtzSent
     xtzCost     <- 0
-    xtzProceeds <- is_i$quote * is_i$xtzSent
+    if (is.na(is_i$xtzProceeds)) xtzProceeds <- is_i$quote * is_i$xtzSent
+    else xtzProceeds <- is_i$xtzProceeds
     
     # Subtract Tezos from balance sheet, calculate tax figures
     for (j in 1:nrow(bs)) {
@@ -209,7 +210,9 @@ for (i in 1:nrow(is)) {
     # Initialize variables
     tokenBalance  <- is_i$tokenSent
     tokenCost     <- 0
-    tokenProceeds <- is_i$xtzReceived * is_i$quote
+    
+    if (is.na(is_i$tokenProceeds)) tokenProceeds <- is_i$xtzReceived * is_i$quote
+    else tokenProceeds <- is_i$tokenProceeds
     j <- 1
     
     # Subtract tokens from balance sheet, calculate tax figures
