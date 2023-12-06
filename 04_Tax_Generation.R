77f7275d4e0273afbb628201172eb76d024aa89c
datcsv <datcsv@gmail.com>
2023-12-06 13:27:36 +0800

    Add logic, placeholders for 2023 tax forms

--- 04_Tax_Generation.R
+++ 04_Tax_Generation.R
@@ -94,17 +94,24 @@ tax_8949_short <- tax_8949 %>% filter(., Short_Term)
 tax_8949_long <- tax_8949 %>% filter(., !Short_Term)
 
 # Generate PDF files
-if (year(date_span[2]) <= 2021) {
+tax_year <- year(date_span[2])
+if (tax_year == 2021) {
   # Define form paths
   f8949   <- "forms/2021/f8949.pdf"
   f1040sd <- "forms/2021/f1040sd.pdf"
   f1040s1 <- "forms/2021/f1040s1.pdf"
   s1_offset <- 0
-} else {
+} else if (tax_year == 2022) {
   # Define form paths
   f8949   <- "forms/2022/f8949.pdf"
   f1040sd <- "forms/2022/f1040sd.pdf"
   f1040s1 <- "forms/2022/f1040s1.pdf"
+  s1_offset <- 5
+} else {
+  # Define form paths
+  f8949   <- "forms/2023/f8949.pdf"
+  f1040sd <- "forms/2023/f1040sd.pdf"
+  f1040s1 <- "forms/2023/f1040s1.pdf"
   s1_offset <- 5
 }
 
